b308cbbd0a589992692c16820858563bedc47a98
Davidson <dehpacifico@gmail.com>
2023-10-29 23:06:19 -0300

    aplicando css no navbar com classe active

--- MINIBLOG/miniblog/src/components/Navbar.jsx
+++ MINIBLOG/miniblog/src/components/Navbar.jsx
@@ -6,13 +6,13 @@ import { NavLink } from 'react-router-dom'
 
 const Navbar = () => {
   return (
-    <nav>
-        <NavLink to='/'>
-            Mini<span><strong>Blog</strong></span>
+    <nav className={styles.navbar}>
+        <NavLink to='/' className={styles.brand}>
+            Mini<span>Blog</span>
         </NavLink>
-        <ul>
-            <li><NavLink to='/'>Home</NavLink></li>
-            <li><NavLink to='/about'>Sobre</NavLink></li>
+        <ul className={styles.link_list}>
+            <li><NavLink to='/' className={({isActive}) => (isActive ? styles.active : '')}>Home</NavLink></li>
+            <li><NavLink to='/about' className={({isActive}) => (isActive ? styles.active : '')}>Sobre</NavLink></li>
         </ul>
     </nav>
   )
