cdd2c805c8d720720f2e8d0fc0810a2bc2ed5849
Davidson <dehpacifico@gmail.com>
2023-10-31 21:38:43 -0300

    mostrando paginas de acordo com autenticacao

--- MINIBLOG/miniblog/src/components/Navbar.jsx
+++ MINIBLOG/miniblog/src/components/Navbar.jsx
@@ -1,23 +1,85 @@
 //CSS
-import styles from './Navbar.module.css'
+import styles from "./Navbar.module.css";
+
+//context
+import { useAuthValue } from "../context/AuthContext";
+
+//hook de autenticação
+import { useAuthentication } from "../hooks/useAuthentication";
 
 //Router
-import { NavLink } from 'react-router-dom'
+import { NavLink } from "react-router-dom";
 
 const Navbar = () => {
+  const { user } = useAuthValue();
+
   return (
     <nav className={styles.navbar}>
-        <NavLink to='/' className={styles.brand}>
-            Mini<span>Blog</span>
-        </NavLink>
-        <ul className={styles.link_list}>
-            <li><NavLink to='/' className={({isActive}) => (isActive ? styles.active : '')}>Home</NavLink></li>
-            <li><NavLink to='/login' className={({isActive}) => (isActive ? styles.active : '')}>Login</NavLink></li>
-            <li><NavLink to='/register' className={({isActive}) => (isActive ? styles.active : '')}>Registrar</NavLink></li>
-            <li><NavLink to='/about' className={({isActive}) => (isActive ? styles.active : '')}>Sobre</NavLink></li>
-        </ul>
+      <NavLink to="/" className={styles.brand}>
+        Mini<span>Blog</span>
+      </NavLink>
+      <ul className={styles.link_list}>
+        <li>
+          <NavLink
+            to="/"
+            className={({ isActive }) => (isActive ? styles.active : "")}
+          >
+            Home
+          </NavLink>
+        </li>
+        {/*páginas para usuario não logado abaixo */}
+        {!user && (
+          <>
+            <li>
+              <NavLink
+                to="/login"
+                className={({ isActive }) => (isActive ? styles.active : "")}
+              >
+                Login
+              </NavLink>
+            </li>
+            <li>
+              <NavLink
+                to="/register"
+                className={({ isActive }) => (isActive ? styles.active : "")}
+              >
+                Registrar
+              </NavLink>
+            </li>
+          </>
+        )}
+        {/*páginas para usuario logado abaixo */}
+        {user && (
+          <>
+            <li>
+              <NavLink
+                to="/posts/create"
+                className={({ isActive }) => (isActive ? styles.active : "")}
+              >
+                Novo Post
+              </NavLink>
+            </li>
+            <li>
+              <NavLink
+                to="/dashboard"
+                className={({ isActive }) => (isActive ? styles.active : "")}
+              >
+                Dashboard
+              </NavLink>
+            </li>
+          </>
+        )}
+        <li>
+          <NavLink
+            to="/about"
+            className={({ isActive }) => (isActive ? styles.active : "")}
+          >
+            Sobre
+          </NavLink>
+        </li>
+      </ul>
     </nav>
-  )
-}
+  );
+};
 
-export default Navbar
+export default Navbar;
---
+++ MINIBLOG/miniblog/src/pages/CreatePost/CreatePost.jsx
@@ -0,0 +1,14 @@
+//css
+import styles from './CreatePost.module.css'
+
+import React from 'react'
+
+const CreatePost = () => {
+  return (
+    <div>
+      <h1>CreatePost</h1>
+    </div>
+  )
+}
+
+export default CreatePost
