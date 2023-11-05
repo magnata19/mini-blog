f402f522ce4c37b19acd5ba03ec059ee6cd8ab3d
Davidson <dehpacifico@gmail.com>
2023-11-04 23:15:03 -0300

    finalizando metodo de pesquisa

--- MINIBLOG/miniblog/src/pages/Search/Search.jsx
+++ MINIBLOG/miniblog/src/pages/Search/Search.jsx
@@ -5,13 +5,31 @@ import styles from './Search.module.css'
 import { useFetchDocuments } from '../../hooks/useFetchDocuments'
 import { useQuery } from '../../hooks/useQuery'
 
+//components
+import PostDetail from '../../components/PostDetail'
+
+//router 
+import { Link } from 'react-router-dom'
+
 const Search = () => {
   const query = useQuery()
   const search = query.get('q')
+
+  const {documents: posts} = useFetchDocuments('posts', search)
   return (
-    <div>
+    <div className={styles.search_container}>
       <h2>Search</h2>
-      <p>{search}</p>
+      <div>
+        {posts && posts.length === 0 && (
+          <div className={styles.noposts}>
+            <p>Não foram encontrados posts em sua busca...</p>
+            <Link to='/' className='btn btn-dark'>Voltar</Link>
+          </div>
+        )}
+        {posts && posts.map((post) => (
+          <PostDetail key={post.id} post={post} />
+        ))}
+      </div>
     </div>
   )
 }
