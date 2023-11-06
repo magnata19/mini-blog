5482fe50d333990208f18126c035cdb1ab7cbaed
Davidson <dehpacifico@gmail.com>
2023-11-06 20:56:13 -0300

    estruturando pagina dashboard

--- MINIBLOG/miniblog/src/pages/Dashboard/Dashboard.jsx
+++ MINIBLOG/miniblog/src/pages/Dashboard/Dashboard.jsx
@@ -1,12 +1,34 @@
 //CSS
 import styles from './Dashboard.module.css'
 
-import React from 'react'
+//router
+import { Link } from 'react-router-dom'
+
+//hooks
+import {useAuthValue} from '../../context/AuthContext'
+import { useFetchDocuments } from '../../hooks/useFetchDocuments'
 
 const Dashboard = () => {
+  const {user} = useAuthValue()
+  const uid = user.uid
+
+  //posts do usuario
+  const posts = []
+
   return (
     <div>
-      <h1>Dashboard</h1>
+      <h2>Dashboard</h2>
+      <p>Gerencia seus posts.</p>
+      {posts && posts.length === 0 ? (
+        <div className={styles.noposts}>
+          <p>Não foram encontrados posts.</p>
+          <Link to='/posts/create' className='btn'>Criar Post</Link>
+        </div>
+      ) : (
+        <div>
+          <p>Tem posts!</p> 
+        </div>
+      )}
     </div>
   )
 }
