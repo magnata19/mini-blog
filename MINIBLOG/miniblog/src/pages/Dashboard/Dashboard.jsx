e8a8bf588cd31166dbbf94c0698de1aac7ba15e4
Davidson <dehpacifico@gmail.com>
2023-11-06 22:20:24 -0300

    finalizando dashboard

--- MINIBLOG/miniblog/src/pages/Dashboard/Dashboard.jsx
+++ MINIBLOG/miniblog/src/pages/Dashboard/Dashboard.jsx
@@ -1,36 +1,65 @@
 //CSS
-import styles from './Dashboard.module.css'
+import styles from "./Dashboard.module.css";
 
 //router
-import { Link } from 'react-router-dom'
+import { Link } from "react-router-dom";
 
 //hooks
-import {useAuthValue} from '../../context/AuthContext'
-import { useFetchDocuments } from '../../hooks/useFetchDocuments'
+import { useAuthValue } from "../../context/AuthContext";
+import { useFetchDocuments } from "../../hooks/useFetchDocuments";
 
 const Dashboard = () => {
-  const {user} = useAuthValue()
-  const uid = user.uid
+  const { user } = useAuthValue();
+  const uid = user.uid;
 
-  //posts do usuario
-  const posts = []
+  const { documents: posts, loading } = useFetchDocuments("posts", null, uid);
+
+  const deleteDocument = (id) => {};
+
+  if(loading) {
+    return <p>Carregando...</p>
+  }
 
   return (
-    <div>
+    <div className={styles.dashboard}>
       <h2>Dashboard</h2>
       <p>Gerencia seus posts.</p>
       {posts && posts.length === 0 ? (
         <div className={styles.noposts}>
           <p>Não foram encontrados posts.</p>
-          <Link to='/posts/create' className='btn'>Criar Post</Link>
+          <Link to="/posts/create" className="btn">
+            Criar Post
+          </Link>
         </div>
       ) : (
-        <div>
-          <p>Tem posts!</p> 
-        </div>
+        <>
+          <div className={styles.post_header}>
+            <span>Título</span>
+            <span>Ações</span>
+          </div>
+          {posts && posts.map((post) => (
+            <div key={post.id} className={styles.post_row}>
+              <p>{post.title}</p>
+              <div>
+                <Link to={`/posts/${post.id}`} className="btn btn-outline">
+                  Ver
+                </Link>
+                <Link to={`posts/edit/${post.id}`} className="btn btn-outline">
+                  Editar
+                </Link>
+                <button
+                  onClick={() => deleteDocument(post.id)}
+                  className="btn btn-outline btn-danger"
+                >
+                  Excluir
+                </button>
+              </div>
+            </div>
+          ))}
+        </>
       )}
     </div>
-  )
-}
+  );
+};
 
-export default Dashboard
+export default Dashboard;
